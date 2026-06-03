c71a4470a0ebe3854a0ecf52763440628b359647
Tanin <tanin.ta1385@gmail.com>
2026-06-03 14:23:38 +0330

    Add database header and sqlite source files

---
+++ Database.h
@@ -0,0 +1,21 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+#include <string>
+#include "sqlite3.h"
+using namespace std;
+
+class Database{
+    private:
+    sqlite3 *Db;
+    bool Connection;
+
+    public:
+    Database(const string &fileName);
+    ~Database();
+    bool Open() const;
+    bool Apply(const string &Sq);
+    bool Query(const string &Sq,int (*Call)(void *, int, char **, char **), void *data);
+    sqlite3 *getDb();
+
+}
+#endif
