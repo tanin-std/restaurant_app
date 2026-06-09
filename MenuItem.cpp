367502ef1e9a7760bc0a7736087d97bc75ba8104
Tanin <tanin.ta1385@gmail.com>
2026-06-09 13:24:19 +0330

    Add MenuItem base class

---
+++ MenuItem.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include "MenuItem.h"
+using namespace std;
+
+menuItem::menuItem(int ID, int resID, string Name,
+        string DES, double Price, 
+            string Type, bool Available)
+        :id(ID), restaurantID(resID),
+        name(Name), desc(DES),
+        basePrice(Price), type(Type), available(Available){
+
+        }
+menuItem::~menuItem(){
+
+}
+int menuItem::getID() const{
+    return id;
+}
+string menuItem::getName() const{
+     return name;
+}
+string menuItem::getDescript() const{
+    return desc;
+}
+int menuItem::getRestaurantID() const{
+    return restaurantID;
+}
+double menuItem::getPrice() const{
+    return basePrice;
+}
+string menuItem::getType() const{
+    return type;
+}
+bool menuItem::getAvailable() const{
+    return available;
+}
+void menuItem::setName(const string &newName){
+    name = newName;
+}
+void menuItem::setPrice(double Price){
+    basePrice = Price;
+}
+void menuItem::setAvailable(bool Available){
+     available = Available;
+}
+double menuItem::FinalPrice() const{
+    return basePrice;
+}
+void menuItem::display() const{
+    cout << "   ?? " << name << " - " << basePrice << endl;
+    cout << desc << endl;
+    if(available){
+        cout << "Available"<< endl;
+    }
+    else{
+        cout << "Unavailable"<< endl;
+    }
+}
---
+++ MenuItem.h
@@ -0,0 +1,37 @@
+#ifndef MENUITEM_H
+#define MENUITEM_H
+#include<string>
+#include<iostream>
+using namespace std;
+
+class menuItem{
+    protected:
+    int id;
+    string name;
+    int restaurantID;
+    string desc;
+    double basePrice;
+    string type;
+    bool available;
+
+    public:
+    menuItem(int ID = 0, int resID = 0,string Name = "",
+        string DES = "", double Price = 0, 
+            string Type = "", bool Available = true);
+    virtual ~menuItem();
+
+    int getID() const;
+    string getName() const;
+    string getDescript() const;
+    int getRestaurantID() const;
+    double getPrice() const;
+    string getType() const;
+    bool getAvailable() const;
+    void setName(const string &newName);
+    void setPrice(double Price);
+    void setAvailable(bool Available);
+    virtual double FinalPrice() const;
+    virtual void display() const;
+
+};
+#endif
