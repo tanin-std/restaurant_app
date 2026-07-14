ec4cc05610703a1ccae5343d74ce726d85201696
Tanin <tanin.ta1385@gmail.com>
2026-07-14 23:33:45 +0330

    Add VipLevel class

---
+++ VIPLevel.cpp
@@ -0,0 +1,8 @@
+#include "VIPLevel.h"
+#include<iostream>
+using namespace std;
+
+VIPLevel::VIPLevel() 
+    : MembershipLevel("VIP", 700, 2.0, 15.0, 0.0, "Free delivery, priority orders"){
+
+    }
---
+++ VIPLevel.h
@@ -0,0 +1,9 @@
+#ifndef VIPLEVEL_H
+#define VIPLEVEL_H
+#include "MembershipLevel.h"
+
+class VIPLevel : public MembershipLevel{
+public:
+    VIPLevel();
+};
+#endif
