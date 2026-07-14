a30bb861fde966a9a3dcf4ae3b5244e9fb5ad06e
Tanin <tanin.ta1385@gmail.com>
2026-07-14 17:54:59 +0330

    Add NormalLevel class

---
+++ NormalLevel.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include "NormalLevel.h"
+
+NormalLevel::NormalLevel() 
+    : MembershipLevel("Normal", 0, 1.0, 0.0, 1.0, "No special benefits"){
+        
+    }
---
+++ NormalLevel.h
@@ -0,0 +1,10 @@
+#ifndef NORMALLEVEL_H
+#define NORMALLEVEL_H
+#include<iostream>
+#include "MembershipLevel.h"
+
+class NormalLevel : public MembershipLevel{
+public:
+    NormalLevel();
+};
+#endif
