c119e82bb60527b298cde6efb1d355911cd513d0
Tanin <tanin.ta1385@gmail.com>
2026-07-14 18:03:07 +0330

    Add Silverlevel class

---
+++ SilverLevel.cpp
@@ -0,0 +1,7 @@
+#include "SilverLevel.h"
+#include<iostream>
+
+SilverLevel::SilverLevel() 
+    : MembershipLevel("Silver", 100, 1.2, 5.0, 1.0, "Exclusive coupons"){
+	
+	}
---
+++ SilverLevel.h
@@ -0,0 +1,10 @@
+#ifndef SILVERLEVEL_H
+#define SILVERLEVEL_H
+#include "MembershipLevel.h"
+
+class SilverLevel : public MembershipLevel{
+public:
+    SilverLevel();
+};
+
+#endif
