d24fe3c63d0523773d5292c93d242c2e7f3f72ee
Tanin <tanin.ta1385@gmail.com>
2026-07-14 18:04:35 +0330

    Added GoldLevel class

---
+++ GoldLevel.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include "GoldLevel.h"
+
+GoldLevel::GoldLevel() 
+    : MembershipLevel("Gold", 300, 1.5, 10.0, 0.5, "Half-price delivery, special items"){
+
+    }
---
+++ GoldLevel.h
@@ -0,0 +1,9 @@
+#ifndef GOLDLEVEL_H
+#define GOLDLEVEL_H
+#include "MembershipLevel.h"
+
+class GoldLevel : public MembershipLevel{
+public:
+    GoldLevel();
+};
+#endif
