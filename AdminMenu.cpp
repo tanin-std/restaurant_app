b90ff63815bc5806381a3ef646aa3404f225b376
Tanin <tanin.ta1385@gmail.com>
2026-07-15 22:39:23 +0330

    Update AdminMenu for phase 2

--- AdminMenu.cpp
+++ AdminMenu.cpp
@@ -7,13 +7,15 @@ AdminMenu::AdminMenu(int id, string username, string password)
     : User(id, username, password, "admin", 0),
      restaurantDAO(nullptr),
       userDAO(nullptr), 
-      orderDAO(nullptr){
+      orderDAO(nullptr),
+	  loyaltyDAO(nullptr){
 
       }
-void AdminMenu::setDAO(RestaurantDAO *rDAO, UserDAO *uDAO, OrderDAO *oDAO){
+void AdminMenu::setDAO(RestaurantDAO *rDAO, UserDAO *uDAO, OrderDAO *oDAO, LoyaltyDAO *lDAO){
     restaurantDAO = rDAO;
     userDAO = uDAO;
     orderDAO = oDAO;
+    loyaltyDAO = lDAO;
 }
 void AdminMenu::showAllRestaurants(){
     vector<Restaurant> restaurants = restaurantDAO->findAll();
@@ -76,29 +78,151 @@ double totalSales = orderDAO->getTotalSales();
     cout << "Total sales: " << totalSales << " Tomans\n";
 }
 
+static int levelCall(void* data, int argc, char** argv, char** col){
+    if(argc >= 2){
+        string level;
+        if(argv[0] != nullptr){
+            level = argv[0];
+        } 
+        else{
+            level = "Unknown";
+        }
+        int count;
+        if(argv[1] != nullptr){
+            count = atoi(argv[1]);
+        } 
+        else{
+            count = 0;
+        }
+        cout << level << ": " << count << " users" << endl;
+    }
+    return 0;
+}
+void AdminMenu::showLevel(){
+    cout << "\n-- Level Statistics --\n";
+    if(loyaltyDAO == nullptr){
+        cout << "Loyalty system not available.\n";
+        return;
+    }
+    string Sql = "SELECT membership_level, COUNT(*) FROM users GROUP BY membership_level;";
+    loyaltyDAO->executeQuery(Sql, levelCall, nullptr);
+}
+void AdminMenu::manageLevel(){
+    int userId, points;
+    string newLevel;
+    cout << "\n--Manual Level Management--\n";
+    cout << "Enter user ID: ";
+    cin >> userId;
+    cout << "Enter new level (Normal/Silver/Gold/VIP): ";
+    cin >> newLevel;
+    cout << "Enter new points: ";
+    cin >> points;
+    if(newLevel != "Normal" && newLevel != "Silver" &&
+        newLevel != "Gold" && newLevel != "VIP"){
+        cout << "Invalid level!\n";
+        return;
+    }
+    if(points < 0){
+        cout << "Points cannot be negative\n";
+        return;
+    }
+    if (loyaltyDAO == nullptr) {
+    cout << "Loyalty system not available\n";
+    return;
+    }
+
+  LoyaltySystem* oldLoyalty = loyaltyDAO->load(userId);
+   string oldLevel;
+
+  if(oldLoyalty != nullptr){
+    oldLevel = oldLoyalty->getLevel();
+  } 
+  else{
+    oldLevel = "Normal";
+}
+bool levelUpdate = loyaltyDAO->updateLevel(userId, newLevel);
+bool pointsUpdate = loyaltyDAO->updatePoints(userId, points);
+    if(levelUpdate && pointsUpdate){
+        loyaltyDAO->LevelChange(userId, oldLevel, newLevel, "admin");
+        cout << "User level updated successfully!\n";
+        cout << "User ID: " << userId << endl;
+        cout << "New Level: " << newLevel << endl;
+        cout << "New Points: " << points << endl;
+    } 
+	else{
+        cout << "Update failed" << endl;
+    }
+    delete oldLoyalty;
+}
+
+void AdminMenu::LevelHistory(){
+    int userId;
+    cout << "\nLevel Change History: \n";
+    cout << "Enter user ID: ";
+    cin >> userId;
+    if(loyaltyDAO == nullptr){
+        cout << "Loyalty system not available\n";
+        return;
+    }
+    vector<string> history = loyaltyDAO->getHistory(userId);
+    if(history.empty()){
+        cout << "No history found for user " << userId << endl;
+        return;
+    }
+    cout << "\nHistory for user " << userId << endl;
+    int i;
+    for(i = 0; i < history.size(); i++){
+        cout << (i + 1) << history[i] << endl;
+    }
+}
 void AdminMenu::showMenu(){
     int choice;
-    do{
-        cout << "\n------System Admin Menu------\n";
+    do {
+        cout << "\n----System Admin Menu----\n";
         cout << "1. View all restaurants\n";
         cout << "2. Add a new restaurant\n";
         cout << "3. Activate/Deactivate a restaurant\n";
         cout << "4. View reports\n";
+        cout << "5. Level statistics\n";         
+        cout << "6. Manual level management\n";  
+        cout << "7. View level history\n";         
         cout << "0. Logout\n";
         cout << "Choice: ";
         cin >> choice;
         switch(choice){
-            case 1: showAllRestaurants(); 
-            break;
-            case 2: addNewRestaurant(); 
-            break;
-            case 3: toggleRestaurantStatus(); 
-            break;
-            case 4: showReports(); 
-            break;
-            case 0: cout << "Logging out...\n"; 
-            break;
-            default: cout << "Invalid choice.\n";
+            case 1: {
+            	showAllRestaurants();
+                break;
+            }
+            case 2:{
+            	addNewRestaurant();
+                break;
+            }
+            case 3:{
+            	toggleRestaurantStatus();
+                break;
+            }
+            case 4:{
+            	showReports();
+                break;
+            }
+            case 5:{
+            	showLevel();
+                break;
+            }
+            case 6:{
+            	manageLevel();
+                break;
+            }
+            case 7:{
+            	LevelHistory();
+                break; 
+            }
+            case 0:{
+            	cout << "Logging out..\n";
+                break;
+            }
+            default: cout << "Invalid choice\n";
         }
     } while(choice != 0);
 }
--- AdminMenu.h
+++ AdminMenu.h
@@ -6,20 +6,26 @@
 #include "RestaurantDAO.h"
 #include "UserDAO.h"
 #include "OrderDAO.h"
+#include "LoyaltyDAO.h"
 
 class AdminMenu : public User{
 private:
     RestaurantDAO *restaurantDAO;
     UserDAO *userDAO;
     OrderDAO *orderDAO;
+    LoyaltyDAO *loyaltyDAO;
+    
     void showAllRestaurants();
     void addNewRestaurant();
     void toggleRestaurantStatus();
     void showReports();
+    void showLevel();
+    void manageLevel();
+    void LevelHistory();
 
 public:
     AdminMenu(int id, string username, string password);
-    void setDAO(RestaurantDAO *rDAO, UserDAO *uDAO, OrderDAO *oDAO);
+    void setDAO(RestaurantDAO *rDAO, UserDAO *uDAO, OrderDAO *oDAO, LoyaltyDAO *lDAO);
     void showMenu() override;
 };
 #endif
